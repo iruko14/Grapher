0db8d45b1e07d30c15dd1e964af369c4ffa6bb5f
Carlos <mendezmercadoc@gmail.com>
2023-12-23 16:47:17 -0400

    Fixing API description

--- API_request.py
+++ API_request.py
@@ -1,21 +1,35 @@
 import requests
 
 def get_categories():
+    # Requesting adress
     r = requests.get('https://api.escuelajs.co/api/v1/categories')
     print("request adress: https://api.escuelajs.co/api/v1/categories")
+
     print("--------------------------------------------------")
+
+    # Checking status code
     print("status code: ",r.status_code)
     if r.status_code == 200:
         print("ENABLE")
     else:
         print("ERROR")
-    print("--------------------------------------------------------------------------------------------------------------")
-    print("page text: ",r.text)
-    print("--------------------------------------------------------------------------------------------------------------")
-    print("data type: ", type(r.text))
-    print("--------------------------------------------------")
-    categories = r.json()
 
+    print("--------------------------------------------------------------------------------------------------------------")
+
+    # Getting page content
+    print("page text: ",r.text)
+
+    print("--------------------------------------------------------------------------------------------------------------")
+
+    # Printing page content data type
+    print("data type: ", type(r.text))
+
+    print("--------------------------------------------------")
+
+    # Translating content to json
+    categories = r.json()
+    # Getting categories name
     for category in categories:
         print("categories: ", category['name'])
+
     print("--------------------------------------------------")
